4d72a4c7bc1908e0c39db46d016383551035d80e
sopiatulaeni2001 <sopiatulaeni93@gmail.com>
2020-11-23 21:22:08 +0800

    Sopiatul Aeni

---
+++ app/src/main/java/simple/example/hewanpedia/model/Kuda.java
@@ -0,0 +1,11 @@
+package simple.example.hewanpedia.model;
+
+
+public class Kuda extends Hewan {
+
+
+    public Kuda(String ras, String asal, String deskripsi, int drawableRes) {
+        super("Kuda",ras,asal,deskripsi,drawableRes);
+    }
+
+}
